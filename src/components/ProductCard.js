b35ccfe230c64b847e652137fcd93f1068098cb8
boolfalse <extravaganza1024@gmail.com>
2023-10-18 16:20:34 -0700

    CartContext usage

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,35 @@
 
-import React from 'react';
-import {Button, Card} from "react-bootstrap";
+import {Button, Card, Col, Form, Row} from "react-bootstrap";
+import {CartContext} from "../CartContext";
+import {useContext} from "react";
 
 export default function ProductCard({product}) {
+    const cart = useContext(CartContext);
+    const productQuantity = cart.getProductQuantity(product.id);
+
     return (
         <Card>
             <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
                 <Card.Text>$ {product.price}</Card.Text>
-                <Button variant='primary'>Add to cart</Button>
+                {
+                    productQuantity > 0 ?
+                        <>
+                            <Form as={Row}>
+                                <Form.Label column='true' sm='6'>
+                                    QTY: {productQuantity}
+                                </Form.Label>
+                                <Col sm='6'>
+                                    <Button onClick={() => cart.addOneToCart(product.id)}
+                                            sm='6' className='mx-2'>+</Button>
+                                    <Button onClick={() => cart.removeOneFromCart(product.id)}
+                                            sm='6' className='mx-2'>-</Button>
+                                </Col>
+                            </Form>
+                            <Button variant='danger' onClick={() => cart.deleteFromCart(product.id)}>Delete</Button>
+                        </> :
+                        <Button variant='primary' onClick={() => cart.addOneToCart(product.id)}>Add to cart</Button>
+                }
             </Card.Body>
         </Card>
     );
